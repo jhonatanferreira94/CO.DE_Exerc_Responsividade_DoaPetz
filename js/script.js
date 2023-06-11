9905db791f2d919bf0eaa3343afb1384ae86e8ed
jhonatanferreira94 <jhonjhonpf@hotmail.com>
2023-06-11 20:05:24 -0300

    Exerc Responsividade

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let menu = document.getElementById("menu_links");
+let menu_barras = document.getElementById("menu_barras");
+
+function mostrarMenu() {
+    if (window.getComputedStyle(menu).display == "none") {
+        menu.style.display = "flex"
+        menu_barras.setAttribute("aria-label", "fechar menu")
+        menu_barras.setAttribute("aria-expanded", "true")
+    } else {
+        menu.style.display = "none"
+        menu_barras.setAttribute("aria-label", "abrir menu")
+        menu_barras.setAttribute("aria-expanded", "false")
+    }
+
+}
+
